ab923b20667fd240d1dccbd7d87cc1f449f3cb3e
fisker <lionkay@gmail.com>
2024-12-20 19:08:47 +0800

    Add "weigang-member-club-sign-in-task" job

---
+++ weigang-member-club-sign-in.mjs
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+import fs from 'node:fs/promises'
+import util from 'node:util'
+import process from 'node:process'
+
+const LOG_FILE = new URL(`./${new Date().toISOString().slice(0,10)}.log`, import.meta.url)
+const {WEIGANG_MEMBER_CLUB_ACCESS_TOKEN: TOKEN} = process.env
+
+async function fetchAndLog() {
+  const response = await fetch.apply(this, arguments);
+  console.log(response)
+  const data = await response.json()
+  console.log(data)
+  return data
+}
+
+async function getSignInActivityId() {
+  const data = await fetchAndLog("https://api-member.4008618618.com/v1/mall/signAct/getCurrentSignAct", {
+    "headers": {
+      "accept": "*/*",
+      "authorization": `Bearer ${TOKEN}`,
+      "cache-control": "no-cache",
+      "content-type": "application/json",
+      "pragma": "no-cache",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "cross-site",
+      "source": "normal"
+    },
+    "referrer": "https://servicewechat.com/wx7fbe0786a008e112/devtools/page-frame.html",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    "credentials": "include"
+  });
+
+  return data.data.signAct.signActId
+}
+
+async function signIn() {
+  const id = await getSignInActivityId()
+
+  if (!id) {
+    return
+  }
+
+  await fetchAndLog("https://api-member.4008618618.com/v1/mall/signAct/sign", {
+    "headers": {
+      "accept": "*/*",
+      "authorization": `Bearer ${TOKEN}`,
+      "cache-control": "no-cache",
+      "content-type": "application/json",
+      "pragma": "no-cache",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "cross-site",
+      "source": "normal"
+    },
+    "referrer": "https://servicewechat.com/wx7fbe0786a008e112/devtools/page-frame.html",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": JSON.stringify({actId: id}),
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "include"
+  });
+}
+
+await signIn()
